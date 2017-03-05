35e68700d1de42118ca15278263b072526d79a42
Brian Knox <taotetek@gmail.com>
2017-03-05 12:54:01 -0500

    Problem: C does not have namespaces

--- go/libmockdiscgo.go
+++ go/libmockdiscgo.go
@@ -3,8 +3,8 @@ package main
 import "C"
 import "fmt"
 
-//export DiscoverEndpoints
-func DiscoverEndpoints(url, key string) *C.char {
+//export ZDiscgoDiscoverEndpoints
+func ZDiscgoDiscoverEndpoints(url, key string) *C.char {
 	return C.CString(fmt.Sprintf("inproc://%s-%s", url, key))
 }
 
